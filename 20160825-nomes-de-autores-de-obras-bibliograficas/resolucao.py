e5aa61a459c7adc9167cc24fc54b3944cef91329
Élysson MR <elyssonmr@gmail.com>
2016-08-25 14:09:12 -0300

    Passo 3 - Refatoração do método formatar_nome

--- 20160825-nomes-de-autores-de-obras-bibliograficas/resolucao.py
+++ 20160825-nomes-de-autores-de-obras-bibliograficas/resolucao.py
@@ -2,9 +2,11 @@ from unittest import TestCase, main
 
 
 def formatar_nome(nome):
-    if len(nome.split()) == 1:
-        return nome.upper()
-    return "SILVA, João"
+    palavras = nome.split()
+    if len(palavras) == 1:
+        return palavras[0].upper()
+    restante = " ".join(palavras[:-1])
+    return "{}, {}".format(palavras[-1].upper(), restante)
 
 
 class SobreNomeAutorTestCase(TestCase):
@@ -12,6 +14,10 @@ class SobreNomeAutorTestCase(TestCase):
     def test_formatar_nome_simples(self):
         resposta = formatar_nome("João Silva")
         self.assertEqual("SILVA, João", resposta)
+
+    def test_formatar_nome_simples_rezende(self):
+        resposta = formatar_nome("Élysson Mendes Rezende")
+        self.assertEqual("REZENDE, Élysson Mendes", resposta)
 
     def test_formatar_nome_uma_palavra(self):
         resposta = formatar_nome("Guimaraes")
