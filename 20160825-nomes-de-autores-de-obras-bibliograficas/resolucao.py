73438f1481152b9d1492fe53dad618d8226a7533
Élysson MR <elyssonmr@gmail.com>
2016-08-25 14:03:06 -0300

    Passo 2

--- 20160825-nomes-de-autores-de-obras-bibliograficas/resolucao.py
+++ 20160825-nomes-de-autores-de-obras-bibliograficas/resolucao.py
@@ -2,6 +2,8 @@ from unittest import TestCase, main
 
 
 def formatar_nome(nome):
+    if len(nome.split()) == 1:
+        return nome.upper()
     return "SILVA, João"
 
 
@@ -10,6 +12,10 @@ class SobreNomeAutorTestCase(TestCase):
     def test_formatar_nome_simples(self):
         resposta = formatar_nome("João Silva")
         self.assertEqual("SILVA, João", resposta)
+
+    def test_formatar_nome_uma_palavra(self):
+        resposta = formatar_nome("Guimaraes")
+        self.assertEqual("GUIMARAES", resposta)
 
 
 if __name__ == "__main__":
