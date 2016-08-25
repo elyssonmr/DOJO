275f037610070ead372994558f566e8fe2d118f6
Élysson MR <elyssonmr@gmail.com>
2016-08-25 16:27:15 -0300

    Passo 4 - Adiciona a regra do penultimo e ultimo nome serem maíusculos

--- 20160825-nomes-de-autores-de-obras-bibliograficas/resolucao.py
+++ 20160825-nomes-de-autores-de-obras-bibliograficas/resolucao.py
@@ -5,6 +5,10 @@ def formatar_nome(nome):
     palavras = nome.split()
     if len(palavras) == 1:
         return palavras[0].upper()
+    if palavras[-1].upper() in ["FILHO", "FILHA", "NETO", "NETA", "SOBRINHO", "SOBRINHA", "JUNIOR"]:
+        restante = " ".join(palavras[:-2])
+        maiusculo = " ".join([palavras[-2].upper(), palavras[-1].upper()])
+        return "{}, {}".format(maiusculo, restante)
     restante = " ".join(palavras[:-1])
     return "{}, {}".format(palavras[-1].upper(), restante)
 
@@ -22,6 +26,10 @@ class SobreNomeAutorTestCase(TestCase):
     def test_formatar_nome_uma_palavra(self):
         resposta = formatar_nome("Guimaraes")
         self.assertEqual("GUIMARAES", resposta)
+
+    def test_formatar_nome_sobrenome_filho(self):
+        resposta = formatar_nome("Élysson Mendes Rezende Filho")
+        self.assertEqual("REZENDE FILHO, Élysson Mendes", resposta)
 
 
 if __name__ == "__main__":
